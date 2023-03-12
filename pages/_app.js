0cd2fa4581959bda7d9a79d1a7e2f6074f99daaa
Ibrahim <ibrahimcoderx@gmail.com>
2023-03-13 02:18:46 +0600

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,38 @@
-import '@/styles/globals.css'
+
+// font
+
+
+
+// Common Css
+import "bootstrap/dist/css/bootstrap.min.css";
+import "/styles/common.css";
+import "/styles/order_section.css";
+import "/styles/common_component.css";
+
+
+
+// landing Css
+// import "../styles/landing_one/landing_one.css";
+
+import "/styles/landing-14/landing-14.css";
+
+
+
+// import '../styles/theme_two/theme_two_media.css';
+
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return  (
+  
+   
+
+        <Component {...pageProps} />
+
+   
+    
+    )
+
 }
+
